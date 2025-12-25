f62195275bb988d34666dd54a9ec008f598ad9d8
Danya <danyaputilin@yandex.ru>
2025-12-25 16:14:20 +0300

    installation update

---
+++ MacOS&Linux_installation.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# ====== Установка игры "Demoniac" ======
+
+# ------- АВТОР -------
+# Putilin Daniil -> "Demoniac" v1.1.0
+# https://github.com/Devilord-coder
+
+# ----- ИНСТРУКЦИЯ -----
+# Запустите этот файл с помощью sudo
+
+
+# Ссылка на проект
+APP_LINK="https://github.com/Devilord-coder/Demoniac"
+DEST_DIR="~/Downloads"
+
+
+echo "======= Starting installation ======="
+echo ""
+
+mkdir app_repo_clone
+cd app_repo_clone
+git clone "$APP_LINK"
+cd Demoniac
+
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+
+pyinstaller Demoniac.spec
+mv dist/Demoniac.app ~/Downloads
+cd ~
+rm Documents/GitHub/app_repo_clone
+
+echo ""
+echo "====================================="
+echo "Succesfully installed Demoniac v1.1.0"
---
+++ Windows_installation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# ====== Установка игры "Demoniac" ======
+
+# ------- АВТОР -------
+# Putilin Daniil -> "Demoniac" v1.1.0
+# https://github.com/Devilord-coder
+
+# ----- ИНСТРУКЦИЯ -----
+# Запустите этот файл с помощью sudo
+
+
+...
